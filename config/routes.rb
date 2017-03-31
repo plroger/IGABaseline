1908d1855336311914e06e29c30400066f2b2164
plroger <plrogerio@gmail.com>
2017-03-31 00:10:07 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   #get 'startup/index'
 
   resources :iga_baselines
-  get 'iga_baselines/index'
+  root 'iga_baselines#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
